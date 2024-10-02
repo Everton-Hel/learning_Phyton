420181a4556bcd598b9883a74f81f26c7f557808
Everton Heleno <everton-hel@hotmail.com>
2024-10-02 00:01:56 -0300

    02-10-2024 - Add introdução de variavel

--- 1_Py_Learne_Inicio.py
+++ 1_Py_Learne_Inicio.py
@@ -5,6 +5,10 @@ def main():
     # ':' -> Declara o inicio de um novo nível de escopo
     pass
 # ----
-main()
 # Executa ao programa
-# 
+main()
+# O phython funciona por "Identação" Executa primeiro o mais a esquerda possível de cima para baixo
+# Escopo pai
+    # Escopo filho
+        # Escopo neto
+            # Quarto bisneto...
---
+++ 2_Py_Learne_Variavel.py
@@ -0,0 +1,19 @@
+def main():
+    # Uma variável é um nome que se refere a u valor
+    # Uma Atribuição - Comando de atribuição cria uma nova variável e lhe dá um valor (set -> Setando -> Atribuindo um valor)
+    # Tipos Primitivos
+        # int/long
+        # float
+        # bool – True/False
+        # Str - String
+        # Tuple - Registros
+        # list – Lista de objetos
+        # dict – Coleção chave/valor
+        # set – Coleção arbitrária
+    pass
+main()
+#Exercicio 2.1 (Calcule a multiplicação enrte 2 valores setados)
+a = 5
+b = 6
+r = a + b
+print('O resultado de a='5'+ b=' 6 ' ='11)
