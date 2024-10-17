9fbd0cd19268deff2e6f1fff8fa14aa737878a5b
Everton Heleno <77762930+Everton-Hel@users.noreply.github.com>
2024-10-17 13:41:09 -0300

    Update 5_Py_Learn_Listas.py
    add Funções nativas para listas

--- 5_Py_Learn_Listas.py
+++ 5_Py_Learn_Listas.py
@@ -1,4 +1,18 @@
 def main():
+  # Funções Nativas para listas
+    # numero.append(3) -> Insere elemento na lista
+    # numero.index(2) -> Retorna o indice de um determinado elemento
+    # numero.insert(1,"nome2") -> Insere um elemento na lista em uma determinada posição
+    # print('Aluno' in Lista_Aluno) -> Verifica se o elemento está ou não na lista
+    # animais.remove('Cachorro') -> Remove um determinado elemento
+    # animais.pop(2) -> Remove um elemento em um determinado indice
+    # del(animais[2] -> Remove o elemento passo em parâmetro
+    # print(sorted([lista_z]) -> Realisa a ordenação da lista
+    # ou
+    # lista_z = lista.sort() -> Realisa a ordenação da lista
+    # ou
+    # lista_z = Sorted(lista) -> Realisa a ordenação da lista
+  
   # Na lista a posição inicial começa no indíce "0"
   lista1 = ["nome1", "nome2", "nome3"]
   print(lista1[0], lista1[1], lista1[2])
@@ -33,12 +47,20 @@ def main():
   print(elemento1, elemento2, elemento3)
 
   # Utilizando a lista da posição X até posição Y
-  Lista_tudo = [1,2,3,4,5,6,7,8,9,0}
+  Lista_tudo = [1,2,3,4,5,6,7,8,9,0]
   teste1 = Lista_tudo[1:4]
   teste2 = Lista_tudo[:4]
   teste3 = Lista_tudo[2:]
   print(teste1, teste2, teste3)
   
+  # Juntando Listas (Join)
+  lista_c = [1, 2, 3]
+  lista_d = [1,2,3,4,5,6,7,8,9,0]
+  lista_e += lista_c + lista_d
+  print(lista_e)
 
+  
+  
+  
 
 main()
