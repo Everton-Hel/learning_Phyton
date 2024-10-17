5013be9435dd6bcc744870e85cc3716366979020
Everton Heleno <77762930+Everton-Hel@users.noreply.github.com>
2024-10-17 17:55:35 -0300

    Update 4_Py_Learne_operadores_aritmeticos.py
    add Booleano

--- 4_Py_Learne_operadores_aritmeticos.py
+++ 4_Py_Learne_operadores_aritmeticos.py
@@ -30,7 +30,7 @@ print('Divisão inteira', divisao_inteira)
 exponenciacao = 2 ** 10
 print('Exponenciação', exponenciacao)
 
-#modulo
+# modulo
 modulo = 55 % 2  # resto da divisão
 print('Módulo', modulo)
 
@@ -39,3 +39,8 @@ print(16 % 8 == 0)
 print(10 % 2 == 0)
 print(15 % 2 == 0)
 print(16 % 2 == 0)
+
+# Booleano
+boolean = True
+boolean = false
+
