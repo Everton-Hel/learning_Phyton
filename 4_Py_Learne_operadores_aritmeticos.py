21a8b30782b2eee2ced7d75708a16d4eeaa83b72
Everton Heleno <77762930+Everton-Hel@users.noreply.github.com>
2024-10-17 15:11:32 -0300

    Update 4_Py_Learne_operadores_aritmeticos.py
    Add Precedencia de operadores aritméticos

--- 4_Py_Learne_operadores_aritmeticos.py
+++ 4_Py_Learne_operadores_aritmeticos.py
@@ -1,21 +1,36 @@
+# Precedencia de operadores aritméticos
+  # 1. (n + n)
+  # 2. **
+  # 3. * / // %
+  # 4. + -
+conta_1 = (1 + int(0.5 + 0.5)) ** (5 + 5)
+print(conta_1)
+
+# adicao
 adicao = 10 + 10
 print('Adição', adicao)
 
+# subtracao
 subtracao = 10 - 5
 print('Subtração', subtracao)
 
+# multiplicacao
 multiplicacao = 10 * 10
 print('Multiplicação', multiplicacao)
 
+# divisao
 divisao = 10 / 3  # float
 print('Divisão', divisao)
 
+#divisao_inteira
 divisao_inteira = 10 // 3
 print('Divisão inteira', divisao_inteira)
 
+# exponenciacao
 exponenciacao = 2 ** 10
 print('Exponenciação', exponenciacao)
 
+#modulo
 modulo = 55 % 2  # resto da divisão
 print('Módulo', modulo)
 
