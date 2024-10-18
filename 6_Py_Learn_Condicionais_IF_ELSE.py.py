1c4917963d618e9634c52825a9dbb19a44016dc7
Everton Heleno <77762930+Everton-Hel@users.noreply.github.com>
2024-10-18 12:00:30 -0300

    Create 6_Py_Learn_Condicionais_IF_ELSE.py.py
    add Condicionais_IF_ELSE

---
+++ 6_Py_Learn_Condicionais_IF_ELSE.py.py
@@ -0,0 +1,11 @@
+# Condicionais IF e ELSE => Avalia se uma expressão ou condição é verdadeira, executando a condição do IF caso seja verdadeiro ou Else (caso exista)
+# As condições IF e ELSE precisa estar dentro do escopo
+def main():
+logica = True
+if logica:
+  print("A logica é verdadeira")
+else:
+  print("A logica não é verdadeiro")
+
+  main()
+# 
