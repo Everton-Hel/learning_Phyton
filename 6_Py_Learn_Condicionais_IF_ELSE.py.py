5a18f1d609657ee0a697bf2b8461f764aab143e8
Everton Heleno <77762930+Everton-Hel@users.noreply.github.com>
2024-10-18 14:06:48 -0300

    Update 6_Py_Learn_Condicionais_IF_ELSE.py.py
    Alterando a logica melhorando o exemplo

--- 6_Py_Learn_Condicionais_IF_ELSE.py.py
+++ 6_Py_Learn_Condicionais_IF_ELSE.py.py
@@ -1,13 +1,17 @@
 # Condicionais IF e ELSE => Avalia se uma expressão ou condição é verdadeira, executando a condição do IF caso seja verdadeiro ou Else (caso exista)
 # As condições IF e ELSE precisa estar dentro da identação
+# if / elif      / else
+# se / se não se / se não
 def main():
-logica = True
-if logica:
-  print("A logica é verdadeira")
-elif logica == '':
-  print("A opção está vazia")
+entrada = input('Você quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema')
+elif entrada == 'sair':
+    print('Você saiu do sistema')
 else:
-  print("A logica não é verdadeiro")
+    print('Você não digitou nem entrar e nem sair.')
+
 
   main()
 # 
