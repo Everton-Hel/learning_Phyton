78167ba6a54ccd017d98086d09f850b99662cf96
Everton Heleno <77762930+Everton-Hel@users.noreply.github.com>
2024-10-18 12:05:30 -0300

    Update 6_Py_Learn_Condicionais_IF_ELSE.py.py
    Add elif na condição

--- 6_Py_Learn_Condicionais_IF_ELSE.py.py
+++ 6_Py_Learn_Condicionais_IF_ELSE.py.py
@@ -1,9 +1,11 @@
 # Condicionais IF e ELSE => Avalia se uma expressão ou condição é verdadeira, executando a condição do IF caso seja verdadeiro ou Else (caso exista)
-# As condições IF e ELSE precisa estar dentro do escopo
+# As condições IF e ELSE precisa estar dentro da identação
 def main():
 logica = True
 if logica:
   print("A logica é verdadeira")
+elif logica == '':
+  print("A opção está vazia")
 else:
   print("A logica não é verdadeiro")
 
