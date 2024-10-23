a8c6b3782d032f1d6a13655d91037b750d985d5b
Everton Heleno <77762930+Everton-Hel@users.noreply.github.com>
2024-10-23 13:39:50 -0300

    Create 012_Py_Learn_BlocosRepeticao_FOR.py
    Add Blocos Repeticao FOR

---
+++ 012_Py_Learn_BlocosRepeticao_FOR.py
@@ -0,0 +1,9 @@
+# Para laços de repetição podemos utilizar o comando FOR
+def main():
+    # lista de alunos
+    alunos = ['Aluno1', 'Aluno2', 'Aluno3']
+    # incrementando um novo nome na lista
+    alunos.append('NovoAluno')
+    for ContadorAluno in alunos:
+        print(ContadorAluno)
+main()
