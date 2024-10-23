d43b26f10695af59d0050b83a3c48f527ca0ecbf
Everton Heleno <77762930+Everton-Hel@users.noreply.github.com>
2024-10-23 18:50:07 -0300

    Update 012_Py_Learn_BlocosRepeticao_FOR.py

--- 012_Py_Learn_BlocosRepeticao_FOR.py
+++ 012_Py_Learn_BlocosRepeticao_FOR.py
@@ -6,13 +6,33 @@ def main():
     alunos.append('NovoAluno')
     for ContadorAluno in alunos:
         print(ContadorAluno)
-
+# -------------------------------------------------------------------------------
 # Para utilizarmos um intervalo no FOR (de X até Y) podemos usar a função "RANGE"
 notas = []
 # Com a função range() podemos fazer o bloco FOR interar quantas vezes definirmos
 for i in range(4,11):
     notas.append(i)
-
 for nota in notas:
     print(nota)
+# -------------------------------------------------------------------------------
+    # Todos os possíveis alunos
+    alunos = ['Aluno1', 'Aluno2', 'Aluno3', 'Aluno4']
+
+    # A notas respectivas dos alunos
+    notas = [[8,10],[10,7],[8,9],[5,5]]
+
+    if (len(alunos) == len(notas)):
+        for indice in range(len(alunos)):
+            soma = 0
+            for nota in notas[indice]:
+                soma += nota
+
+            media = soma / len(notas[indice])
+            print(f'O aluno {alunos[indice]} tem a média {media}')
+            if (media > 6):
+                print(f' - Portanto ele foi aprovado')
+            else:
+                print(f' - Portanto ele foi reprovado')
+    else:
+        print("Tem algo errado/nPossivelmente algum aluno não fez a prova")
 main()
