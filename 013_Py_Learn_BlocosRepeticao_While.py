8ace043bdfb662901334735f7043d77ee0ec8d8c
Everton Heleno <everton-hel@hotmail.com>
2024-10-23 23:20:09 -0300

    Add exeplos do While

---
+++ 013_Py_Learn_BlocosRepeticao_While.py
@@ -0,0 +1,18 @@
+# Laço de repetição While, que significa 'enquanto'
+## Funciona enquanto uma condição for verdadeira fazendo uma determinada ação, sendo necessario informar a saida do laço
+## tomar cuidado para nçao entrar em looping infinito
+
+def main():
+    contador = 1
+
+    while contador < 6:
+        print(contador)
+        contador += 1
+    
+# ------------------------------------------------------------------
+    while condicao_de_parada != '9':
+        condicao_de_parada = input('Digite im númro: ')
+        for Cont_i in range(1, int(condicao_de_parada)):
+            print(Cont_i)
+
+main()
