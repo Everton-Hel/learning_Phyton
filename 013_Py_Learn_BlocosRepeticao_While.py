7fde1eba19f80b910a7dcabbc1cd928eeebb1270
Everton Heleno <77762930+Everton-Hel@users.noreply.github.com>
2024-10-24 13:39:31 -0300

    Update 013_Py_Learn_BlocosRepeticao_While.py
    add Break e continue

--- 013_Py_Learn_BlocosRepeticao_While.py
+++ 013_Py_Learn_BlocosRepeticao_While.py
@@ -1,6 +1,8 @@
 # Laço de repetição While, que significa 'enquanto'
 ## Funciona enquanto uma condição for verdadeira fazendo uma determinada ação, sendo necessario informar a saida do laço
 ## tomar cuidado para nçao entrar em looping infinito
+## Continue -> Continua a operação de loop
+## Break -> Quebra a operação de loop
 
 def main():
     contador = 1
@@ -14,5 +16,23 @@ def main():
         condicao_de_parada = input('Digite im númro: ')
         for Cont_i in range(1, int(condicao_de_parada)):
             print(Cont_i)
+
+# ------------------------------------------------------------------
+# O comando Break tem a função de parar o looping e sair
+    i = 1
+    while i < 5:
+        print(i)
+        I += 1
+        if i ==3:
+            break
+# ------------------------------------------------------------------
+# O comando continue tem a função de pular aquela passada do looping e continuar a proxima passada
+    c = 1
+    while c < 5:
+        c += 1
+        if c ==3:
+            continue
+        print(c)
+            
 
 main()
